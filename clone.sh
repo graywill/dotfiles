f708b7d78c7dd09ec7820389be9427dced88e32d
graywill <dave@graywill.com>
2022-06-28 20:46:29 -0700

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -7,14 +7,8 @@ BLADE=$SITES/blade-ui-kit
 LARAVEL=$SITES/laravel
 
 # Personal
-git clone git@github.com:driesvints/driesvints.com.git $SITES/driesvints.com
-git clone git@github.com:driesvints/learn-laravel-cashier.com.git $SITES/learn-laravel-cashier.com
-git clone git@github.com:driesvints/vat-calculator.git $SITES/vat-calculator
-git clone git@github.com:eventyio/eventy.io.git $SITES/eventy.io
-git clone git@github.com:fullstackbelgium/fullstackbelgium.be.git $SITES/fullstackbelgium.be
-git clone git@github.com:fullstackeurope/fullstackeurope.com.git $SITES/fullstackeurope.com
-git clone git@github.com:laravelio/laravel.io.git $SITES/laravel.io
-git clone git@github.com:laravelio/paste.laravel.io.git $SITES/paste.laravel.io
+git clone git@github.com:graywill/leads.git $SITES/leads
+
 
 # Blade UI Kit
 git clone git@github.com:blade-ui-kit/demo.git $BLADE/demo
